de7475f356efd28e4a6485b7ab7b4bc2cf8bc76a
Luiza Santos <luiza.santos1102@ufu.br>
2022-11-11 17:08:12 -0300

    Second commit

---
+++ Lab_1/fibonacci.c
@@ -0,0 +1,27 @@
+/*
+*Arquivo: fibonacci.c
+*Data de criação: 11/nov/2022
+*Autor: Luiza Arruda Santos 
+*Medio 2
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int N, a;
+    int b = 0;
+    int c = 1;
+    int i = 0;
+	printf("Digite o numero de termos desejados:");
+	scanf("%d", &N);
+	while(i < N){
+	    printf("%d, ", b);
+	    a = b + c;
+	    c = b;
+	    b = a;
+	    i++;
+        if(i == N)
+            printf("...");
+    }
+    return 0;
+}
